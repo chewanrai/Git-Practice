f1e64ca9bbcf9c046ba8f974507894a9ffbdefe3
chewanrai <kulung.che14@gmail.com>
2022-05-27 09:45:30 +0545

    Aadarsh

---
+++ Tuple.py
@@ -0,0 +1,38 @@
+#Tuple-- Should be enclosed by paranthesis and comma just after the elements
+
+#Creating Tuple w/o using paranthesis
+# T=1,2,3,4
+
+# # count
+# a= "Pythonn"
+# b= a.count("n")
+# # b= a.index(2)
+# print(b)
+# #Len Functrion
+
+# a= ("Sunil", "Roshan", 29, [ "Sunil", "Akash", 29])
+# print(a)
+# print(len(a))
+# a[3]. pop()
+# print(a)
+# a[3]. append("AMster")
+# print(a)
+# a[3]. remove("Akash")
+# print(a)
+
+# print("Mac is:", max(1,2,3,4,5,8,6))
+# print("Min is:", min(1,2,3,4,5,8,6))
+
+#Concatenation of two tuple
+# a=("sunil", "Roshan", 29)
+# b= ("Akash", "Cheean", 28)
+# print(a+b)
+
+# a=(1,2,3,4,5,6)
+# b= list(a)
+# # b.append(8)
+# # b.insert(2, 9)
+# c= tuple(b)
+# print(type(c))
+# print(c)
+
