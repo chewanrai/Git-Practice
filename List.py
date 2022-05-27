bd4ce237b590f989cf41c05a9bcafc7a6e9328a9
chewanrai <kulung.che14@gmail.com>
2022-05-27 09:42:23 +0545

    Lalit

---
+++ List.py
@@ -0,0 +1,65 @@
+# data= ["SUnil", "Roshan", 29]
+# for i in data:
+#     print(i)
+
+# data = ["SUnil", "Roshan", 29]
+# # print(data[1])
+# print(data[0:1])
+
+# data = list(range(0,10,1))
+# print(data)
+
+# data = [ "Sunil", "Roshan", 29]
+# # # data.append(9)
+# # # data.append("Programming")
+# # # data.insert(1,50)                      ##1 is indexing
+# data2 = ["a", "b"]
+# data2.extend(data)
+# print(data2)
+
+# inserting a tuple(as an element) to the list
+# mixedlist= [{1,2}, [5,6,7], {"a": "r"}]
+# numbertuple= (3,4)
+# mixedlist.insert(1, numbertuple)
+# print("Updated list:", mixedlist)
+
+#Updating the elememts of a list
+# a=[ 1,2,3,4,"Sunil"]
+# a[0]= 5
+# a[1]= 6
+# a[2]= 7
+# a[3]= 8
+# a[4]= "Roshan"
+# print(a)
+
+# Python list del function
+# a=[ 1,2,3,4,"Sunil"]
+# del a[1]
+# print(a)
+
+#Python list remove
+# a=[ 1,2,3,4,"Sunil"]
+# a.remove("Sunil")
+# print(a)
+
+#Python list (last valu lai hatauxa)
+# a=[ 1,2,3,4,"Sunil"]
+# a.pop()
+# print(a)
+
+#Concatenation of two list
+# a= ["Sunil", "Roshan", "Chewan"]
+# b= ["a", "2"]
+# print(a+b)
+
+# Python Copy
+# a= ["Sunil", "Roshan", "Chewan"]
+# b=[]
+# b=[5]
+# b= a.copy()
+# print(b)
+
+# Python list clear
+# a= ["Sunil", "Roshan", "Chewan"]
+# a.clear()
+# print(a)
