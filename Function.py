4af2ea3c54c6ef461674c98da5ed33ce7fd2df98
chewanrai <kulung.che14@gmail.com>
2022-05-27 09:35:49 +0545

    Chewan

---
+++ Function.py
@@ -0,0 +1,67 @@
+# def login():
+#     print("python")
+# login()
+
+# def function():
+#     print("python\n" * 5)
+# def function2():
+#     print("Hellow world")
+
+# function()
+# function2()
+
+# def function():
+#     print("python" * 5)
+# def function2():
+#     print("Hellow world")
+
+# function()
+# function2()
+
+# def add():
+#     x=10
+#     y=20
+#     c=x+y
+#     print(c)
+# add()
+
+# def login(username,password):
+#     print(f"your username is: {username} and your password is:{ password}")
+# login("sunil", "BAttle")
+
+# def multiply(a,b):
+#     print(a*b)
+# multiply(2,3)
+
+# def pw (x,y):
+#     z=x**y
+#     print(z)
+
+# pw(5,2)
+
+# def show(name,age):
+#     print(f"NAme: {name} Age: {age}")
+# show(name="Sunil", age="20")
+
+# def show(name, age=20):
+#     print(name,age)
+# show(name="SUnil")
+
+# def show(name, age=20):
+#     print(name,age)               #Garbage collection
+# show("SUnil", 30)
+
+# def show(name, age=20):
+#     print(name,age)                 #Garbage collection
+# show(name="SUnil", age=30)
+
+# def show(*num):
+#     z=num[0]+num[1]+num[2]
+#     print(z)
+# show(5,4,3)
+
+# def show(**num):
+#     z=num["a"]+num["b"]+num["c"]
+#     print(z)
+# show(a=5,b=4,c=3)\
+
