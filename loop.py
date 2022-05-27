26592cd8f974c01573fc08be907442e6a49c56ed
chewanrai <kulung.che14@gmail.com>
2022-05-27 09:44:18 +0545

    Anish

---
+++ loop.py
@@ -0,0 +1,85 @@
+# Write a Python program to reverse a string
+# a="python"
+# for i in range(5,-1,-1):
+#     print(a[i],end="")
+
+# b= ""
+# a= input("Input a word")
+# for i in reversed(a):
+#     b=b+i
+# print(b)
+
+#Sum: 1 to 10 (or any number) using while loop
+
+# sum=0
+# a=1
+# while a<=10:
+#     sum=sum+a
+#     a=a+1
+# print(sum)
+
+
+
+#Write a Python program to print the even numbers from a given list.
+# Sample List:[1,2,3,4,5,6,7,8,9]
+# a=[1,2,3,4,5,6,7,8,9]
+# even
+
+# a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for i in a:
+#     if i % 2 == 0:
+#         print(i)
+
+
+
+
+
+# a=[1,2,3,4]
+# a.append(6)
+# print (a)
+
+# a= [1,2,3,4,5]
+# for i in a:
+#     print(a)
+
+# a= [1,2,3,4,5]
+# for i in a:
+#     print(i)
+
+# a='chewan'
+# for i in a:
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# a="Chewan"
+# for i in reversed(a):
+#     print(i)
+
+# a="Chewan"
+# print(a,end="\n")
+# print("hello")
+
+# a= "Chewan"
+# for i in reversed(a):
+#     print(i, end="")
+
+# b= ""
+# a= input("Input a word")
+# for i in reversed(a):
+#     b=b+i
+# print(b)
+
+# b=[]
+# a=[1,2,3,4,5]
+# for i in reversed(a):
+#     b.append(i)
+#     print(b)
+
+# b=[]
+# a=[1,2,3,4,5]
+# for i in reversed(a):
+#     b.append(i)
+# print(b)
+
